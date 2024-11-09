6c6ba0489d92b3380f83c0bf7a5b815acad1156e
J K Swaminathan <82333513+Swaminathanjk@users.noreply.github.com>
2024-11-09 11:10:25 +0530

    Update firebaseConfig.js

--- amaclone/src/firebaseConfig.js
+++ amaclone/src/firebaseConfig.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth"; // Import Auth
 import { getFirestore } from "firebase/firestore"; // If using Firestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6rWUIQv29Osh0pQ7TYpojI5Ox1YR9Qyo",
-  authDomain: "vite-19f02.firebaseapp.com",
-  projectId: "vite-19f02",
-  storageBucket: "vite-19f02.firebasestorage.app",
-  messagingSenderId: "375744926594",
-  appId: "1:375744926594:web:e602b5e4e4d4d03470b59b",
-  measurementId: "G-1T2CX4C210",
+// your firebase Config from Firebase console
 };
 
 // Initialize Firebase
